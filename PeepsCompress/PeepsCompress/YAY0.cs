e0fc797fb31da3152098b35e2ff392a42b3e59c1
UnknownToaster <wererabbbit@yahoo.com>
2017-05-02 20:01:58 -0500

    Inclusion of YAY0.cs
    -The current csproj file needs the yay0.cs class file in order to open,
    so I have included it for now.

---
+++ PeepsCompress/PeepsCompress/YAY0.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeepsCompress
+{
+    class YAY0 : SlidingWindowAlgorithm
+    {
+        public override byte[] compress(byte[] file, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] decompress(BinaryReader br, int offset, FileStream inputFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] compressInitialization(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] decompressInitialization(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
